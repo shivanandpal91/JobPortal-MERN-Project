fdf68b466a0ca61c91876651a05737e06093cb63
Shivanand Pal <palmanish200@gmail.com>
2025-07-17 12:22:47 +0530

    changing cors setting

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -15,18 +15,18 @@ require('dotenv').config();
 require('./Models/db');
 
 //middleware
-var corsOptions = {
-  origin: ['http://localhost:5173', 'https://job-portal-frontendui.vercel.app'],
-  methods: "GET,POST,PUT,DELETE,OPTIONS",
-  credentials:true,
-  // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// var corsOptions = {
+//   origin: ['http://localhost:5173', 'https://job-portal-frontendui.vercel.app'],
+//   methods: "GET,POST,PUT,DELETE,OPTIONS",
+//   credentials:true,
+//   // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 app.use(express.json({ limit: '5mb' })); // or more, if needed
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
-// app.use(cors());
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use('/auth',AuthRouter);
 app.use('/products',ProductRouter);
